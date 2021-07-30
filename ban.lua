ebc3eb063a6e3668f38821353a2d73b6c41ab367
LORDRUS <71215776+LORDRUS@users.noreply.github.com>
2021-07-30 16:11:55 +0300

    Update ban.lua

--- ban.lua
+++ ban.lua
@@ -1,7 +1,6 @@
 local Plrs = game:GetService("Players")
 local HS = game:GetService("HttpService")
 local List = "1631999051"
-local List = "2450987571"
 Plrs.PlayerAdded:Connect(function(Plr)
     pcall(function()
         local Bans = HS:GetAsync(List)
