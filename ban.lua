46b9238f15b4d4b3c6814ab02752602add64fb4b
LORDRUS <71215776+LORDRUS@users.noreply.github.com>
2021-07-30 16:10:44 +0300

    Update and rename TTEHttpBansHandler.lua to ban.lua

--- ban.lua
+++ ban.lua
@@ -1,14 +1,12 @@
---// hailtt_e
-
 local Plrs = game:GetService("Players")
 local HS = game:GetService("HttpService")
-local List = "" -- Rawed global ban list URL here.
-
+local List = "1631999051"
+local List = "2450987571"
 Plrs.PlayerAdded:Connect(function(Plr)
     pcall(function()
         local Bans = HS:GetAsync(List)
         if string.find(Bans, Plr.UserId) then
-            Plr:Kick("You have been http-banned!")
+            Plr:Kick("You have been banned!")
         end
     end)
 end)
