d011c6c6f3c62c18fe6ac01771b117f03ae1379c
LORDRUS <71215776+LORDRUS@users.noreply.github.com>
2021-07-30 16:09:04 +0300

    Add files via upload

---
+++ TTEHttpBansHandler.lua
@@ -0,0 +1,14 @@
+--// hailtt_e
+
+local Plrs = game:GetService("Players")
+local HS = game:GetService("HttpService")
+local List = "" -- Rawed global ban list URL here.
+
+Plrs.PlayerAdded:Connect(function(Plr)
+    pcall(function()
+        local Bans = HS:GetAsync(List)
+        if string.find(Bans, Plr.UserId) then
+            Plr:Kick("You have been http-banned!")
+        end
+    end)
+end)
